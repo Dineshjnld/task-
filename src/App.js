92890bf0acabc63b8bfc9c4b028bf666e1f8e60d
Dineshjnld <dineshjnld22@gmail.com>
2024-01-05 06:11:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from './components/table'; // Assuming the file name is 'Table.js'
+import './App.css'; // Import your CSS file for styling
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://sample1-5c2m.onrender.com/data');
+        setData(response.data);
+        setLoading(false); // Set loading to false when data is fetched
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Set loading to false on error as well
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Scheduler Data</h1>
+        <br></br>
+        <p>Welcome to the Scheduler application. Below is the table displaying scheduling data.</p>
+       <br></br>
+       <p>The Schedule is from 31-12-2023 to 06-12-2023</p>
+      </div>
+
+      {loading ? (
+        // Display a loading spinner while data is being fetched
+        <div className="loading-spinner">Loading...</div>
+      ) : (
+        // Render the table component once data is available
+        <div>
+          <div className="table-container">
+            <Table data={data} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/table.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import { format, parseISO, isSameDay } from 'date-fns';
+
+const Table = ({ data }) => {
+  const [date, setDate] = useState();
+  const [sortedData, setSortedData] = useState(data);
+
+  useEffect(() => {
+    if (date) {
+      const selectedDate = parseISO(date);
+      const sorted = data.filter((item) => {
+        const meetingStartTime = parseISO(item.bookedServicesData[0].meetingStartTime);
+        return isSameDay(meetingStartTime, selectedDate);
+      });
+      setSortedData(sorted);
+    } else {
+      setSortedData(data);
+    }
+  }, [data, date]);
+
+  const getCurrentDateTime = () => new Date();
+
+  const getReminderStatus = (firstReminderTime, secondReminderTime) => {
+    const currentTime = getCurrentDateTime();
+    if (currentTime > parseISO(secondReminderTime)) {
+      return 'success';
+    } else if (currentTime > parseISO(firstReminderTime) && currentTime < parseISO(secondReminderTime)) {
+      return 'waiting';
+    } else {
+      return 'waiting';
+    }
+  };
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-5">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-700 dark:text-gray-400 table-auto bg-white dark:bg-gray-800">
+        <thead className="text-xs text-white uppercase bg-slate-700 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Sno
+            </th>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Meeting Start Time <br />
+              <input type="date" onChange={(e) => setDate(e.target.value)} />
+            </th>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Doctor name
+            </th>
+            
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Customer Name
+            </th>
+            
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              1st Reminder Sent Time
+            </th>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              2nd Reminder Sent Time
+            </th>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Delivery Channel
+            </th>
+            <th scope="col" className="px-6 py-3 sm:py-2">
+              Is Reminder()
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.map((data, index) => (
+            <tr
+              key={data.accId}
+              className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+            >
+              <td className="px-6 py-4 sm:py-2">{index + 1}</td>
+              <td className="px-6 py-4 sm:py-2">
+                {format(parseISO(data.bookedServicesData[0].meetingStartTime), 'MMM d yyyy h:mma')}
+              </td>
+              <th className="px-6 py-4 sm:py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {data.Username_doctor}
+              </th>
+              {/* <td className="px-6 py-4 sm:py-2">{data.doctorEmail}</td> */}
+              <td className="px-6 py-4 sm:py-2">{data.bookedServicesData[0].customerName}</td>
+              {/* <td className="px-6 py-4 sm:py-2">{data.bookedServicesData[0].customerEmail}</td> */}
+              <td className="px-6 py-4 sm:py-2">
+                {format(
+                  new Date(parseISO(data.bookedServicesData[0].meetingStartTime).getTime() - 60 * 60 * 1000),
+                  'MMM d yyyy h:mma'
+                )}
+              </td>
+              <td className="px-6 py-4 sm:py-2">
+                {format(
+                  new Date(parseISO(data.bookedServicesData[0].meetingStartTime).getTime() - 5 * 60 * 1000),
+                  'MMM d yyyy h:mma'
+                )}
+              </td>
+              <td className="px-6 py-4 sm:py-2">Email</td>
+              <td className="px-6 py-4 sm:py-2">
+                {getReminderStatus(
+                  data.bookedServicesData[0].meetingStartTime,
+                  format(
+                    new Date(parseISO(data.bookedServicesData[0].meetingStartTime).getTime() - 5 * 60 * 1000),
+                    'yyyy-MM-dd HH:mm:ss'
+                  )
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
